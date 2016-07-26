81d7d564868bac20966a21fb5c4b80eec3a13b9a
Mark Redeman <markredeman@gmail.com>
2016-07-26 16:30:20 +0200

    Add test for generating mutatations

---
+++ tests/GenerateMutationsTest.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Renamed\Tests;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use PhpParser\Lexer;
+use PhpParser\ParserFactory;
+use PhpParser\Node;
+use Renamed\GenerateMutations;
+use Renamed\MutationOperator;
+use Renamed\Mutation;
+
+class GenerateMutationsTest extends TestCase
+{
+    private $ast;
+
+    /**
+     * @before
+     */
+    public function setupDefaultAST()
+    {
+        $this->ast = $this->generateASTFromCode('<?php 1 + 2 + 3;');
+    }
+
+    /** @test */
+    function it_cant_create_mutatins_if_no_mutation_operators_have_been_applied()
+    {
+        $generator = new GenerateMutations;
+
+        $this->assertEmpty($generator->generate($this->ast));
+    }
+
+    /** @test */
+    function it_generates_mutations_from_a_given_AST()
+    {
+        // Use a null operator, that it it changes each node to `null`
+        $operator = new class implements MutationOperator {
+                public function mutate(Node $node)
+                {
+                    yield null;
+                }
+            };
+
+        $generator = new GenerateMutations($operator);
+        $mutations = $generator->generate($this->ast);
+
+        // Check if the types are correct
+        $this->assertContainsOnlyInstancesOf(Mutation::class, $mutations);
+
+        // Now check that each node will be mutated
+        foreach ($mutations as $mutation) {
+            $this->assertNull($mutation->mutation());
+        }
+    }
+
+    private function generateASTFromCode(string $code) : array
+    {
+        $lexer = new Lexer(['usedAttributes' => ['startline', 'endline']]);
+        $parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7, $lexer);
+        return $parser->parse($code);
+    }
+}
